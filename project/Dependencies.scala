fb968a1adbf0aa61d726c499e51f7a3c78cb752c
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2019-08-29 18:20:59 +0300

    fix dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,30 +38,29 @@ object Dependencies {
     val magnolia = "0.11.0"
   }
 
-  val catsCore        = "org.typelevel"              %% "cats-core"        % Version.cats
-  val catsFree        = "org.typelevel"              %% "cats-free"        % Version.cats
-  val monocle         = "com.github.julien-truffaut" %% "monocle-core"     % Version.monocle
-  val alleycats       = "org.typelevel"              %% "alleycats-core"   % Version.cats
-  val catsEffect      = "org.typelevel"              %% "cats-effect"      % Version.catsEffect
-  val monix           = "io.monix"                   %% "monix"            % Version.monix
-  val simulacrum      = "com.github.mpilquist"       %% "simulacrum"       % Version.simulacrum
-  val logback         = "ch.qos.logback"             % "logback-classic"   % Version.logback
-  val slf4j           = "org.slf4j"                  % "slf4j-simple"      % Version.slf4j % Provided
-  val circeCore       = "io.circe"                   %% "circe-core"       % Version.circe
-  val circeJava8      = "io.circe"                   %% "circe-java8"      % Version.circe
-  val circeDerivation = "io.circe"                   %% "circe-derivation" % Version.circe
-  val scalatest       = "org.scalatest"              %% "scalatest"        % Version.scalatest % Test
-  val scalamock       = "org.scalamock"              %% "scalamock"        % Version.scalamock % Test
+  val catsCore        = "org.typelevel"              %% "cats-core"           % Version.cats
+  val catsFree        = "org.typelevel"              %% "cats-free"           % Version.cats
+  val monocle         = "com.github.julien-truffaut" %% "monocle-core"        % Version.monocle
+  val alleycats       = "org.typelevel"              %% "alleycats-core"      % Version.cats
+  val catsEffect      = "org.typelevel"              %% "cats-effect"         % Version.catsEffect
+  val monix           = "io.monix"                   %% "monix"               % Version.monix
+  val simulacrum      = "com.github.mpilquist"       %% "simulacrum"          % Version.simulacrum
+  val logback         = "ch.qos.logback"             % "logback-classic"      % Version.logback
+  val slf4j           = "org.slf4j"                  % "slf4j-simple"         % Version.slf4j % Provided
+  val circeCore       = "io.circe"                   %% "circe-core"          % Version.circe
+  val circeJava8      = "io.circe"                   %% "circe-java8"         % Version.circe
+  val circeDerivation = "io.circe"                   %% "circe-derivation"    % Version.circe
+  val magnolia        = "com.propensive"             %% "magnolia"            % Version.magnolia
+  val derevo          = "org.manatki"                %% "derevo-core"         % Version.derevo
+  val derevoTagless   = "org.manatki"                %% "derevo-cats-tagless" % Version.derevo
+  val enumeratum      = "com.beachape"               %% "enumeratum"          % Version.enumeratum
+  val fs2             = "co.fs2"                     %% "fs2-io"              % Version.fs2
+  val tethys          = "com.tethys-json"            %% "tethys-core"         % Version.tethys
+  val tethysJackson   = "com.tethys-json"            %% "tethys-jackson"      % Version.tethys
+  val catsTagless     = "org.typelevel"              %% "cats-tagless-macros" % Version.catsTagless
 
-  val derevo        = "org.manatki"   %% "derevo-core"         % Version.derevo
-  val derevoTagless = "org.manatki"   %% "derevo-cats-tagless" % Version.derevo
-  val enumeratum    = "com.beachape"  %% "enumeratum"          % Version.enumeratum
-  val catsTagless   = "org.typelevel" %% "cats-tagless-macros" % Version.catsTagless
-
-  val fs2 = "co.fs2" %% "fs2-io" % Version.fs2
-
-  val tethys        = "com.tethys-json" %% "tethys-core"    % Version.tethys
-  val tethysJackson = "com.tethys-json" %% "tethys-jackson" % Version.tethys
+  val scalatest = "org.scalatest" %% "scalatest" % Version.scalatest % Test
+  val scalamock = "org.scalamock" %% "scalamock" % Version.scalamock % Test
 
   val macros = Keys.libraryDependencies ++= {
     minorVersion.value match {
