db59d1a5042589be5813d9db24077777d757c983
Z1kkurat <kshelopugin@gmail.com>
2019-12-30 16:59:01 +0300

    Add docs for effect-delaying functions

--- env/src/main/scala/tofu/env/bio/EnvBioFunctions.scala
+++ env/src/main/scala/tofu/env/bio/EnvBioFunctions.scala
@@ -15,11 +15,35 @@ private[bio] trait EnvBioFunctions extends EnvBioProducts { self: EnvBio.type =>
   def pure[A](x: A): EnvBio[Any, Nothing, A]       = fromTask(Task.pure(x))
   def raiseError[E](e: E): EnvBio[Any, E, Nothing] = fromTask(Task.raiseError(UserError(e)))
 
-  def context[R]: EnvBio[R, Nothing, R]                    = Task.now
-  def fromTask[R, A](task: Task[A]): EnvBio[R, Nothing, A] = _ => task
-  def delayTotal[Any, A](x: => A): EnvBio[Any, Nothing, A] =
+  /** Returns current context, embedded in `EnvBio`.
+    * Example:
+    * {{{
+    *   import scala.concurrent.duration.Duration
+    *   import tofu.env.bio.EnvBio
+    *
+    *   val env: EnvBio[String, Nothing, String] =
+    *     for {
+    *       _   <- EnvBio.unit
+    *       ctx <- EnvBio.context
+    *     } yield ctx
+    *
+    *   env.run("ctx").runSyncUnsafe(Duration.Inf)
+    *   // will result in Right("ctx")
+    * }}}*/
+  def context[R]: EnvBio[R, Nothing, R] = Task.now
+
+  /** Creates `EnvBio` from total, non-failing `Task`.
+    * Any error raised by Task will result in fatal error being thrown. */
+  def fromTask[A](task: Task[A]): EnvBio[Any, Nothing, A] = _ => task
+
+  /** Creates `EnvBio` from total, non-throwing effect, resulting in a value of type `A`.
+    * Any error thrown by `x` will result in fatal error being thrown. */
+  def delayTotal[A](x: => A): EnvBio[Any, Nothing, A] =
     fromTask(Task.delay(x))
-  def delay[Any, A](x: => A): EnvBio[Any, Throwable, A] =
+
+  /** Creates `EnvBio` from total effect.
+    * Any error thrown by `x` will result in failed `EnvBio` fixed to Left side. */
+  def delay[A](x: => A): EnvBio[Any, Throwable, A] =
     fromTask(Task.delay(x).onErrorHandleWith(e => Task.raiseError(UserError(e))))
 
   private[this] val anyUnit: EnvBio[Any, Nothing, Unit] = fromTask(Task.unit)
