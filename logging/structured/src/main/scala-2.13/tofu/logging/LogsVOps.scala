d9fe4ecd660f744165344de05edb93824fa78ffb
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2020-01-09 12:43:29 +0300

    version bump to 0.6.1

--- logging/structured/src/main/scala-2.13/tofu/logging/LogsVOps.scala
+++ logging/structured/src/main/scala-2.13/tofu/logging/LogsVOps.scala
@@ -2,7 +2,7 @@ package tofu.logging
 
 trait LogsVOps[I[_], F[_]] { self: Logs[I, F] =>
   final def named[name <: String with Singleton](
-      name: ValueOf[name]
+      implicit name: ValueOf[name]
   ): I[ServiceLogging[F, name]] =
     byName(name.value).asInstanceOf[I[ServiceLogging[F, name]]]
 
