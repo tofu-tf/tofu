31816dcc4479e0db79bdd469d63d015b323d9429
catostrophe <40268503+catostrophe@users.noreply.github.com>
2020-01-18 01:46:09 +0300

    fix wrong implicit type

--- core/src/main/scala/tofu/Guarantee.scala
+++ core/src/main/scala/tofu/Guarantee.scala
@@ -32,7 +32,7 @@ trait Finally[F[_], Exit[_]] extends Guarantee[F] {
 object Finally extends FinallyInstanceChain[Finally]
 
 trait FinallyInstanceChain[T[f[_], exit[_]] >: Finally[f, exit]] {
-  final implicit def fromBracket[F[_], E](implicit F: Bracket[F, E]): Finally[F, TConst[ExitCase[E], *]] =
+  final implicit def fromBracket[F[_], E](implicit F: Bracket[F, E]): T[F, TConst[ExitCase[E], *]] =
     new Finally[F, TConst[ExitCase[E], *]] {
       def finallyCase[A, B, C](init: F[A])(action: A => F[B])(release: (A, ExitCase[E]) => F[C]): F[B] =
         F.bracketCase(init)(action) {
