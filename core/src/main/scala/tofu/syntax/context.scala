f4225b91dcbe29a7457fe851e4a2f8cffd30df61
sigevsky <nick.sigevsky@gmail.com>
2020-01-19 01:05:38 +0100

    Add askF syntax & extract method for specific context

--- core/src/main/scala/tofu/syntax/context.scala
+++ core/src/main/scala/tofu/syntax/context.scala
@@ -1,14 +1,23 @@
 package tofu.syntax
 
+import cats.Monad
 import tofu._
 
 object context {
   def context[F[_]](implicit ctx: Context[F]): F[ctx.Ctx] = ctx.context
 
+  def context[F[_], C](implicit ctx: F HasContext C): F[C] = ctx.context
+
   def ask[F[_]] = new AskPA[F](true)
 
   class AskPA[F[_]](val __ : Boolean) extends AnyVal {
     def apply[C, A](f: C => A)(implicit ctx: F HasContext C): F[A] = ctx.ask(f)
+  }
+
+  def askF[F[_]] = new AskFPA[F](true)
+
+  class AskFPA[F[_]](val __ : Boolean) extends AnyVal {
+    def apply[C, A](f: C => F[A])(implicit ctx: F HasContext C, M: Monad[F]): F[A] = ctx.askF(f)
   }
 
   def runContext[F[_]] = new RunContextPA[F]
