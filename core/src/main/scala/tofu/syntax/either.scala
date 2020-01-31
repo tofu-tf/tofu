38e5ed475c1fb74a894203effdf07f90d46f0898
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2020-01-31 14:34:06 +0300

    Update Selective typeclass and instances

--- core/src/main/scala/tofu/syntax/either.scala
+++ core/src/main/scala/tofu/syntax/either.scala
@@ -1,7 +1,15 @@
 package tofu.syntax
 
 object either {
-  implicit final class AssocLOps[A, B, C](private val e: Either[Either[A, B], C]) extends AnyVal {
+  implicit final class TofuLeftOps[A, B](private val l: Left[A, B]) extends AnyVal {
+    def coerceR[C]: Left[A, C] = l.asInstanceOf[Left[A, C]]
+  }
+
+  implicit final class TofuRightOps[A, B](private val l: Right[A, B]) extends AnyVal {
+    def coerceL[C]: Right[C, B] = l.asInstanceOf[Right[C, B]]
+  }
+
+  implicit final class TofuEitherAssocLOps[A, B, C](private val e: Either[Either[A, B], C]) extends AnyVal {
     def assocL: Either[A, Either[B, C]] = e match {
       case Left(Left(a))  => Left(a)
       case Left(Right(b)) => Right(Left(b))
@@ -9,7 +17,7 @@ object either {
     }
   }
 
-  implicit final class AssocROps[A, B, C](private val e: Either[A, Either[B, C]]) extends AnyVal {
+  implicit final class TofuEitherAssocROps[A, B, C](private val e: Either[A, Either[B, C]]) extends AnyVal {
     def assocR: Either[Either[A, B], C] = e match {
       case Left(a)         => Left(Left(a))
       case Right(Left(b))  => Left(Right(b))
