dfab3634844d3bbafc9f14c150b69329ec196461
A.Voi <voytsishevskiy@tochka.com>
2020-02-03 18:03:49 +0500

    move summoners to Errors.scala;

--- core/src/main/scala/tofu/package.scala
+++ core/src/main/scala/tofu/package.scala
@@ -3,19 +3,10 @@ import cats.{ApplicativeError, MonadError}
 
 package object tofu {
   type HasContext[F[_], C] = Context[F] { type Ctx = C }
-  object HasContext {
-    def apply[F[_], C](implicit hc: HasContext[F, C]): HasContext[F, C] = hc
-  }
 
   type HasLocal[F[_], C] = Local[F] { type Ctx = C }
-  object HasLocal {
-    def apply[F[_], C](implicit hl: HasLocal[F, C]): HasLocal[F, C] = hl
-  }
 
   type HasContextRun[F[_], G[_], C] = RunContext[F] { type Ctx = C; type Lower[A] = G[A] }
-  object HasContextRun {
-    def apply[F[_], G[_], C](implicit hcr: HasContextRun[F, G, C]): HasContextRun[F, G, C] = hcr
-  }
 
   type ApplicativeThrow[F[_]] = ApplicativeError[F, Throwable]
   type MonadThrow[F[_]]       = MonadError[F, Throwable]
