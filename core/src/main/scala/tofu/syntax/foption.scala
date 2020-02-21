7b4b5d16a009bab8728da0ee82be6ea6f16d7704
d1skort <d1skortneru@yandex.ru>
2020-02-21 21:11:44 +0300

    add someF and noneF method to foption object

--- core/src/main/scala/tofu/syntax/foption.scala
+++ core/src/main/scala/tofu/syntax/foption.scala
@@ -1,18 +1,24 @@
 package tofu.syntax
 
-import cats.Monad
+import cats.{Applicative, Monad}
 import cats.syntax.option._
 import tofu.Raise
 import tofu.syntax.monadic._
 
 object foption {
-  implicit final class FOptionOps[F[_], A](private val lhs: F[Option[A]]) extends AnyVal {
+  def noneF[F[_]: Applicative, A]: F[Option[A]] = none[A].pure[F]
+
+  implicit final class FOptionOps[A](private val a: A) extends AnyVal {
+    def someF[F[_]: Applicative]: F[Option[A]] = a.some.pure[F]
+  }
+
+  implicit final class FOptionSyntax[F[_], A](private val lhs: F[Option[A]]) extends AnyVal {
     def getOrElseF(fa: => F[A])(implicit F: Monad[F]): F[A] =
       lhs.flatMap(_.fold(fa)(_.pure[F]))
     def orElseF(fa: => F[Option[A]])(implicit F: Monad[F]): F[Option[A]] =
       lhs.flatMap {
         case None => fa
-        case x => x.pure[F]
+        case x    => x.pure[F]
       }
     def orThrow[E](err: => E)(implicit F: Monad[F], FE: Raise[F, E]): F[A] =
       lhs.getOrElseF(FE.raise(err))
--- core/src/test/scala/tofu/syntax/FOptionSuite.scala
+++ core/src/test/scala/tofu/syntax/FOptionSuite.scala
@@ -1,75 +1,68 @@
 package tofu.syntax
 
-import cats.Monad
-import cats.syntax.option._
-import cats.syntax.applicative._
 import cats.instances.list._
 import cats.instances.either._
 import org.scalatest.flatspec.AnyFlatSpec
 import tofu.syntax.foption._
 
 class FOptionSuite extends AnyFlatSpec {
-  def someF[F[_]: Monad]: F[Option[Int]] = 42.some.pure[F]
-
-  def noneF[F[_]: Monad]: F[Option[Int]] = none[Int].pure[F]
-
   "getOrElseF" should "return inner value when non empty" in {
     assert(
-      someF[List].getOrElseF(List(1, 1, 1)) === List(42)
+      42.someF[List].getOrElseF(List(1, 1, 1)) === List(42)
     )
   }
 
-  "getOrEsleF" should "return argument when empty" in {
+  "getOrElseF" should "return argument when empty" in {
     assert(
-      noneF[List].getOrElseF(List(1, 1, 1)) === List(1, 1, 1)
+      noneF[List, Int].getOrElseF(List(1, 1, 1)) === List(1, 1, 1)
     )
   }
 
   "orElseF" should "return self when non empty" in {
     assert(
-      someF[List].orElseF(List[Option[Int]](Some(1), Some(1), Some(1))) === List(Some(42))
+      42.someF[List].orElseF(List[Option[Int]](Some(1), Some(1), Some(1))) === List(Some(42))
     )
   }
 
   "orElseF" should "return argument when empty" in {
     assert(
-      noneF[List].orElseF(List[Option[Int]](Some(1), Some(1), Some(1))) === List(Some(1), Some(1), Some(1))
+      noneF[List, Int].orElseF(List[Option[Int]](Some(1), Some(1), Some(1))) === List(Some(1), Some(1), Some(1))
     )
   }
 
   "orThrow" should "return inner value when non empty" in {
     assert(
-      someF[Either[String, *]].orThrow("empty") === Right(42)
+      42.someF[Either[String, *]].orThrow("empty") === Right(42)
     )
   }
 
   "orThrow" should "raise error when empty" in {
     assert(
-      noneF[Either[String, *]].orThrow("empty") === Left("empty")
+      noneF[Either[String, *], Int].orThrow("empty") === Left("empty")
     )
   }
 
   "flatMapOpt" should "run when non empty" in {
     assert(
-      someF[List].flatMapOpt(x => List(x + 1)) === List(Some(43))
+      42.someF[List].flatMapOpt(x => List(x + 1)) === List(Some(43))
     )
   }
 
   "flatMapOpt" should "keep empty when empty" in {
     assert(
-      noneF[List].flatMapOpt(x => List(x + 1)) === List(None)
+      noneF[List, Int].flatMapOpt(x => List(x + 1)) === List(None)
     )
   }
 
   "doubleFlatMap" should "run when non empty" in {
     assert(
-      someF[List].doubleFlatMap(x => List(Some(x + 1))) === List(Some(43))
+      42.someF[List].doubleFlatMap(x => List(Some(x + 1))) === List(Some(43))
     )
   }
 
   "doubleFlatMap" should "keep empty when empty" in {
     assert(
-      noneF[List].doubleFlatMap(x => List(Some(x + 1))) === List(None)
+      noneF[List, Int].doubleFlatMap(x => List(Some(x + 1))) === List(None)
     )
   }
 }
