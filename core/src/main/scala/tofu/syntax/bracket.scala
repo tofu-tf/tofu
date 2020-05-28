660b5bfaf1b3101ab25cbba11a462e37bf118fef
Rat King <mehakun1@gmail.com>
2020-05-28 19:15:53 +0300

    rename methods, implement guarnteeOpt

--- core/src/main/scala/tofu/syntax/bracket.scala
+++ core/src/main/scala/tofu/syntax/bracket.scala
@@ -28,20 +28,23 @@ object bracket {
     def guaranteeAlways[B](release: F[B])(implicit F: Applicative[F], FG: Guarantee[F]): F[A] =
       FG.bracket(F.unit)(_ => fa)((_, _) => release)
 
-    def guaranteeOpt[B](use: A => F[A])(release: A => F[B])(implicit FG: Guarantee[F], A: Applicative[F]): F[A] =
+    def guaranteeOpt[B](release: Boolean => F[B])(implicit F: Applicative[F], FG: Guarantee[F]): F[A] =
+      FG.bracket(F.unit)(_ => fa)((_, success) => release(success))
+
+    def bracketTransact[B](use: A => F[A])(release: A => F[B])(implicit FG: Guarantee[F], A: Applicative[F]): F[A] =
       FG.bracket(FG.bracket(fa)(use) {
         case (oldA, success) => release(oldA).whenA(!success)
       })(newA => release(newA).map(_ => newA)) { (_, _) => A.unit }
 
-    def guaranteeModifyOpt[B, C](
+    def bracketOpt[B, C](use: A => F[B])(release: (A, Boolean) => F[C])(implicit FG: Guarantee[F]): F[B] =
+      FG.bracket(fa)(use)(release)
+
+    def bracketState[B, C](
         use: A => F[(A, B)]
-    )(release: A => F[C])(implicit FG: Guarantee[F], A: Applicative[F]): F[B]                            =
+    )(release: A => F[C])(implicit FG: Guarantee[F], A: Applicative[F]): F[B]                                    =
       FG.bracket(FG.bracket(fa)(use) {
         case (oldA, success) => release(oldA).whenA(!success)
       }) { case (newA, b) => release(newA).map(_ => b) } { (_, _) => A.unit }
-
-    def bracketOpt[B, C](use: A => F[B])(release: (A, Boolean) => F[C])(implicit FG: Guarantee[F]): F[B] =
-      FG.bracket(fa)(use)(release)
 
     def finallyCase[Ex[_], B, C](use: A => F[B])(release: (A, Ex[B]) => F[C])(implicit FG: Finally[F, Ex]): F[B] =
       FG.finallyCase(fa)(use)(release)
@@ -55,7 +58,7 @@ object bracket {
       * @return `F[A]` modified value contained in `MVar`
       */
     def bracketUpdate(use: A => F[A])(implicit FG: Guarantee[F], A: Applicative[F]): F[A] =
-      mvar.take.guaranteeOpt(use)(mvar.put)
+      mvar.take.bracketTransact(use)(mvar.put)
 
     /**
       * Modify value with effectful transformation, calculating result. In case of error value remains unchanged
@@ -63,7 +66,7 @@ object bracket {
       * @return `F[B]`
       */
     def bracketModify[B](use: A => F[(A, B)])(implicit FG: Guarantee[F], A: Applicative[F]): F[B] =
-      mvar.take.guaranteeModifyOpt(use)(mvar.put)
+      mvar.take.bracketState(use)(mvar.put)
   }
 
   implicit final class TofuBracketEitherTOps[F[_], E, A](private val e: EitherT[F, E, A]) extends AnyVal {
