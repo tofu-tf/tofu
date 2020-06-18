d9efd233b1043cc9ab7b32da81f5cec25558217b
Aleksei Terekhin <daron666@yandex.ru>
2020-06-18 21:46:06 +0400

    Closes TinkoffCreditSystems/tofu#112

---
+++ core/src/test/scala/tofu/ErrorsSuite.scala
@@ -0,0 +1,26 @@
+package tofu
+
+import cats.instances.either.catsStdInstancesForEither
+import cats.syntax.either._
+import org.scalatest.EitherValues
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ErrorsSuite extends AnyFlatSpec with Matchers with EitherValues {
+
+  "Raise" should "reRaise" in {
+    val err: String = "oops"
+    Raise[ErrorOr, String].reRaise(toErr[ErrorOr[Unit]](err)) shouldBe toErr[Unit](err)
+  }
+
+  "Errors" should "adoptError" in {
+    val err: String                         = "oops"
+    val pf: PartialFunction[String, String] = { case s if s == err => err + "_" + err }
+    val fa: ErrorOr[Unit]                   = Raise[ErrorOr, String].raise(err)
+    Errors[ErrorOr, String].adaptError(fa)(pf) shouldBe toErr(pf(err))
+  }
+
+  type ErrorOr[A] = Either[String, A]
+
+  private def toErr[A](err: String): ErrorOr[A] = err.asLeft[A]
+}
