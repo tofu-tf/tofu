4f243bffe537fd0f26a1c34d881b7e7be41205a6
oskin1 <ilya.arcadich@gmail.com>
2020-06-21 20:59:33 +0300

    Abstract chunks.

--- streams/src/main/scala/tofu/streams/Compile.scala
+++ streams/src/main/scala/tofu/streams/Compile.scala
@@ -1,5 +1,13 @@
 package tofu.streams
 
-trait Compile[F[_], G[_], C[_]] {
+trait Compile[F[_], G[_]] {
+
+  type C[_]
+
   def compile[A](fa: F[A]): G[C[A]]
+}
+
+object Compile {
+
+  type Aux[F[_], G[_], CH[_]] = Compile[F, G] { type C[x] = CH[x] }
 }
--- streams/src/test/scala/example.scala
+++ streams/src/test/scala/example.scala
@@ -1,10 +1,11 @@
-import cats.FlatMap
+import cats.{FlatMap, Functor}
 import cats.effect.ExitCode
 import cats.instances.list._
 import tofu.common.Console
 import tofu.streams._
 import tofu.streams.syntax.compile._
 import tofu.streams.syntax.evals._
+import tofu.streams.syntax.chunks._
 import tofu.syntax.monadic._
 
 object example {
@@ -14,18 +15,22 @@ object example {
   }
 
   object Srv {
-    def apply[F[_], S[_]: Evals[*[_], F]]: Srv[S] = new Impl[F, S]
-    final class Impl[F[_], S[_]](implicit S: Evals[S, F]) extends Srv[S] {
-      override def requestAll: S[String]          = S.evals(getIds).evalMap(requestById)
+    def apply[F[_], S[_]: Evals[*[_], F]: Chunks[*[_], C], C[_]: Functor]: Srv[S] = new Impl[F, S, C]
+    final class Impl[F[_], S[_], C[_]: Functor](implicit S: Evals[S, F], C: Chunks[S, C]) extends Srv[S] {
+      override def requestAll: S[String]          = S.evals(getIds).mapChunks(_.map(_ % 2)).evalMap(requestById)
       private def getIds: F[List[Int]]            = ???
       private def requestById(id: Int): F[String] = ???
     }
   }
 
-  final class App[F[_]: Console: FlatMap, S[_]: Evals[*[_], F]: Compile[*[_], F, Vector]] {
+  final class App[
+    F[_]: Console: FlatMap,
+    S[_]: Evals[*[_], F]: Compile.Aux[*[_], F, Vector]: Chunks[*[_], C],
+    C[_]: Functor
+  ] {
 
     def run: F[ExitCode] = {
-      val srv = Srv[F, S]
+      val srv = Srv[F, S, C]
       srv.requestAll.compile >>= (xs => Console[F].putStr(xs.mkString(", ")) as ExitCode.Success)
     }
   }
