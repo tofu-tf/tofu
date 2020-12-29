4c56c51dd1037ecb43341f32f7ce88bd3f9ffa66
Антон Войцишевский <grygry0@gmail.com>
2020-12-30 00:39:00 +0500

    add docs on logging annotations;

--- logging/derivation/src/main/scala/tofu/logging/derivation/masking.scala
+++ logging/derivation/src/main/scala/tofu/logging/derivation/masking.scala
@@ -7,8 +7,11 @@ import magnolia.Param
 
 sealed trait MaskMode
 object MaskMode {
+  /** Replaces each digit with "#" and each letter with "*". Other symbols are not changed*/
   case object Full                                       extends MaskMode
+  /** Replaces value with "..." */
   case object Erase                                      extends MaskMode
+  /** Same as [[Full]] but for some part of value */
   case class ForLength(offset: Int, maxLength: Int = -1) extends MaskMode {
     def this(length: Int) = this(0, length)
   }
