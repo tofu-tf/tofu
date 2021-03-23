1fce9fefe0c2b189e0948eb1fce654093eb55fbc
Oleg Nizhnik <odomontois@gmail.com>
2021-03-23 17:38:44 +0300

    Log parameters by name

--- logging/structured/src/main/scala/tofu/logging/LoggingMid.scala
+++ logging/structured/src/main/scala/tofu/logging/LoggingMid.scala
@@ -142,6 +142,6 @@ class ArgsLoggable(values: Seq[(String, LoggedValue)]) extends LoggedValue {
   override def toString = values.map { case (name, value) => s"$name = $value" }.mkString("(", ", ", ")")
 
   def logFields[I, V, @specialized R, @specialized M](input: I)(implicit r: LogRenderer[I, V, R, M]): R = {
-    values.foldLeft(r.noop(input)) { (res, p) => r.combine(res, p._2.logFields(input)) }
+    values.foldLeft(r.noop(input)) { (res, p) => r.combine(res, r.field(p._1, input, p._2)) }
   }
 }
