0f17539bb5a5c36b11d14c9c71f0ac835207dae5
catostrophe <40268503+catostrophe@users.noreply.github.com>
2021-07-10 19:52:07 +0300

    Drop build rework leftovers

--- streams/src/main/scala-2.12/tofu/streams/internal/package.scala
+++
@@ -1,7 +0,0 @@
-package tofu.streams
-
-import scala.collection.generic.CanBuildFrom
-
-package object internal {
-  private[tofu] type Factory[-A, +C] = CanBuildFrom[Nothing, A, C]
-}
--- streams/src/main/scala-2.13/tofu/streams/internal/package.scala
+++
@@ -1,5 +0,0 @@
-package tofu.streams
-
-package object internal {
-  private[tofu] type Factory[-A, +C] = scala.collection.Factory[A, C]
-}
