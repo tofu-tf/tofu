b25a458930ea2261796d9780b3b6082153648b29
Антон Войцишевский <31823086+FunFunFine@users.noreply.github.com>
2021-07-14 12:35:19 +0500

    Context remove prepare 3.0 (#702)
    * move members into With* types;
    
    * remove usage of Has* types in internal of covariant places;
    
    * remove unused;
    
    * format WithRun;

--- modules/kernel/src/main/scala/tofu/WithContext.scala
+++ modules/kernel/src/main/scala/tofu/WithContext.scala
@@ -1,7 +1,7 @@
 package tofu
 
-import cats.Functor
-import tofu.optics.Contains
+import cats.{Applicative, FlatMap, Functor}
+import tofu.optics.{Contains, Extract}
 
 /** Synonym for [[Context]] with explicit C as Ctx for better type inference
   *
@@ -13,15 +13,124 @@ import tofu.optics.Contains
   */
 trait WithContext[F[_], C] extends Context[F] {
   override type Ctx = C
+
+  /** Returns context modified by pure function `f`.
+    *
+    * For example one can use it like that:
+    * {{{
+    *   Context[F].ask(myCtx => myCtx.toString)
+    * }}}
+    *
+    * Also there is more convenient syntax:
+    * {{{
+    *   import tofu.syntax.context._
+    *
+    *   ask[F](myCtx => myCtx.toString)
+    * }}}
+    *
+    * @note It does not affect context itself.
+    */
+  override def ask[A](f: Ctx => A): F[A] = functor.map(context)(f)
+
+  /** Same as [[ask]] but `f` is effectual */
+  override def askF[A](f: Ctx => F[A])(implicit F: FlatMap[F]): F[A] = F.flatMap(context)(f)
+
+  /** Allows to focus context on its inside with lens.
+    *
+    * @param extract lens that can extract value of type `A` from `Ctx`
+    */
+  override def extract[A](extract: Extract[Ctx, A]): WithContext[F, A] =
+    new ContextExtractInstance[F, Ctx, A](this, extract)
+
+  override def asWithContext: WithContext[F, C] = this
 }
 
 /** Companion object for [[WithContext]] */
 object WithContext {
   def apply[F[_], C](implicit ctx: WithContext[F, C]): WithContext[F, C] = ctx
+
+  /** Creates constant Context of type C in F
+    *
+    * Allows to put some value into context of F
+    * even when F itself does not have any way to store it
+    *
+    * @example {{{
+    *   case class MyCtx(id: Int)
+    *
+    *   val program: Option[String] = {
+    *     implicit val ctx = Context.const[Option, MyCtx](MyCtx(3))
+    *
+    *     Context[Option].ask(ctx => ctx.id.toString)
+    *   }
+    * }}}
+    */
+  def const[F[_]: Applicative, C](c: C): F WithContext C = new WithContext[F, C] {
+    val functor: Functor[F] = Functor[F]
+    val context: F[C]       = Applicative[F].pure(c)
+  }
+
+  /** Same as const but context is effectual here
+    *
+    * @example {{{
+    *   def handleRequest[F[_]: Monad: *[_] WithContext TraceId](httpRequest: Request) = ???
+    *
+    *   def myProgram[F[_]: Monad] =
+    *     for {
+    *       request                                 <- getRequest[F]
+    *       implicit0(ctx: TraceId In F) <- Context.make(genTraceId[F])
+    *       _                                       <- handleRequest[F](request)
+    *     } yield ()
+    * }}}
+    */
+  def make[F[_]: Functor, A](fa: F[A]): WithContext[F, A] = new WithContext[F, A] {
+    val functor: Functor[F] = implicitly
+    val context: F[A]       = fa
+  }
+
+  /** A mix-in for supplying environment data type companions with useful things
+    *
+    * @example {{{
+    * @ClassyOptics
+    * case class MyContext(id: Int, date: String)
+    *
+    * object MyContext extends Context.Companion[MyContext]
+    * }}}
+    */
+  trait Companion[C] extends ContextInstances[C] {
+
+    type Has[F[_]] = WithContext[F, C]
+
+    implicit def promoteContextStructure[F[_], A](implicit
+        withContext: WithContext[F, C],
+        field: C Contains A
+    ): WithContextContainsInstance[F, C, A] =
+      new WithContextContainsInstance[F, C, A]
+
+    /** Access [[C]] in [[F]].
+      */
+    final def access[F[_]](implicit has: Has[F]): F[C] =
+      has.context
+  }
+
+  trait ContextInstances[C] {
+
+    implicit def promoteLocalStructure[F[_], A](implicit
+        context: WithLocal[F, C],
+        field: C Contains A
+    ): WithLocal[F, A] =
+      context.subcontext(field)
+  }
 }
 
 final class WithContextContainsInstance[F[_], A, B](implicit wc: WithContext[F, A], lens: A Contains B)
     extends WithContext[F, B] {
   def functor: Functor[F] = wc.functor
   def context: F[B]       = wc.extract(lens).context
+}
+
+private[tofu] class ContextExtractInstance[F[_], C1, C2](ctx: F WithContext C1, extract: C1 Extract C2)
+    extends WithContext[F, C2] {
+  def functor: Functor[F] = ctx.functor
+
+  def context: F[C2] = functor.map(ctx.context)(extract.extract)
 }
