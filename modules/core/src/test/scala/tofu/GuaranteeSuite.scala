2bd7847561c5e777f6f25959e086088666a76400
Oleg Nizhnik <o.nizhnikov@tinkoff.ru>
2021-07-20 20:10:36 +0300

    fmt

--- modules/core/src/test/scala/tofu/GuaranteeSuite.scala
+++ modules/core/src/test/scala/tofu/GuaranteeSuite.scala
@@ -9,7 +9,7 @@ object GuaranteeSuite {
   def summonFinally[F[_]] = new SummonSomeFinally[F]
 
   class SummonSomeFinally[F[_]] {
-    def some[Exit[_]](implicit fin: Finally[F, Exit]): Finally[F, Exit]                               = fin
+    def some[Exit[_]](implicit fin: Finally[F, Exit]): Finally[F, Exit]                                 = fin
     def carrier(implicit cc: FinallyCarrier2[F, Throwable]): FinallyCarrier2.Aux[F, Throwable, cc.Exit] = cc
   }
 
