264311c2279e29c71b2cd1d7a611f162dd81d589
Aristov Ivan <apostrof1995@gmail.com>
2021-11-03 16:15:06 +0300

    fior syntax

--- modules/kernel/src/main/scala/tofu/syntax/fior.scala
+++ modules/kernel/src/main/scala/tofu/syntax/fior.scala
@@ -4,7 +4,6 @@ import cats.{Functor, Monad, Semigroup}
 import cats.data.Ior
 import cats.syntax.flatMap._
 import cats.syntax.functor._
-import cats.syntax.traverse._
 
 object fior {
 
@@ -16,8 +15,19 @@ object fior {
     def mapIn[C](f: B => C)(implicit F: Functor[F]): F[Ior[A, C]] =
       fior.map(_.map(f))
 
-    def doubleFlatMap[C](f: B => F[Ior[A, C]])(implicit F: Monad[F], S: Semigroup[A]): F[Ior[A, C]] =
-      fior.flatMap(_.flatTraverse(f))
+    def flatMapIn[AA >: A, C](f: B => Ior[AA, C])(implicit F: Functor[F], s: Semigroup[AA]): F[Ior[AA, C]] =
+      fior.map(_.flatMap(f))
+
+    def doubleFlatMap[AA >: A, C](f: B => F[Ior[AA, C]])(implicit F: Monad[F], S: Semigroup[AA]): F[Ior[AA, C]] =
+      fior.flatMap {
+        case l @ Ior.Left(_) => F.pure(l)
+        case Ior.Right(b) => f(b)
+        case Ior.Both(a0, b) => f(b).map {
+          case Ior.Left(a) => Ior.left(S.combine(a0, a))
+          case Ior.Right(b) => Ior.both(a0, b)
+          case Ior.Both(a, b) => Ior.both(S.combine(a0, a), b)
+        }
+      }
 
     def doubleFlatTap[C](f: B => F[C])(implicit F: Monad[F]): F[Ior[A, B]] =
       fior.flatTap(_.traverse(f))
