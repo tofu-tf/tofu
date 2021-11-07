7f09cb17c99cd15c384edaa48b78041d19cd1a1e
Aristov Ivan <apostrof1995@gmail.com>
2021-11-07 17:24:53 +0300

    tests, fmt

--- modules/core3/src/test/scala/tofu/syntax/FIorSuite.scala
+++ modules/core3/src/test/scala/tofu/syntax/FIorSuite.scala
@@ -8,7 +8,9 @@ import cats.syntax.option.catsSyntaxOptionId
 import cats.syntax.option.none
 import tofu.syntax.fior._
 
-class FIorSuite extends AnyWordSpec with Matchers  {
+import java.util.concurrent.atomic.AtomicInteger
+
+class FIorSuite extends AnyWordSpec with Matchers {
 
   def boom[A] = sys.error("boom")
 
@@ -111,6 +113,128 @@ class FIorSuite extends AnyWordSpec with Matchers  {
 
     "check lazyness" in {
       Ior.left[String, Int]("").some.doubleFlatMap(_ => boom[Option[Ior[String, Int]]]) mustBe Some(Ior.left(""))
+    }
+  }
+
+  "TofuFIorOps#flatTapF" should {
+    "run function well in case of right" in {
+      val atom = new AtomicInteger(1)
+      Ior.right(1).some.flatTapF(i => Some(atom.addAndGet(i))) mustBe Some(Ior.right(1))
+      atom.get() mustBe 2
+    }
+
+    "run function well in case of left" in {
+      val atom = new AtomicInteger(1)
+      Ior.left[String, Int]("foo").some.flatTapF(i => Some(atom.addAndGet(i))) mustBe Some(Ior.left("foo"))
+      atom.get() mustBe 1
+    }
+
+    "run function well in case of both" in {
+      val atom = new AtomicInteger(1)
+      Ior.both("foo", 1).some.flatTapF(i => Some(atom.addAndGet(i))) mustBe Some(Ior.both("foo", 1))
+      atom.get() mustBe 2
+    }
+
+    "nothing happen in case of effect error" in {
+
+      val atom = new AtomicInteger(1)
+      none[Ior[String, Int]].flatTapF(i => Some(atom.addAndGet(i))) mustBe none[Ior[String, Int]]
+      atom.get() mustBe 1
+
+      Ior.right(1).some.flatTapF(_ => none[Any]) mustBe none[Ior[String, Int]]
+      Ior.left[String, Int]("foo").some.flatTapF(_ => none[Any]) mustBe Some(Ior.left("foo"))
+      Ior.both[String, Int]("foo", 1).some.flatTapF(_ => none[Any]) mustBe none[Ior[String, Int]]
+    }
+
+    "check lazyness" in {
+      none[Ior[String, Int]].flatTapF(_ => boom[Option[Any]]) mustBe none[Ior[String, Int]]
+      Ior.left[String, Int]("foo").some.flatTapF(_ => boom[Option[Any]]) mustBe Some(Ior.left("foo"))
+    }
+  }
+
+  "TofuFIorOps#doubleFlatTap" should {
+    "run function well in case of right (to right)" in {
+      val atom = new AtomicInteger(1)
+      Ior.right[String, Int](1).some.doubleFlatTap(i => Ior.right(atom.addAndGet(i)).some) mustBe Some(Ior.right(1))
+      atom.get() mustBe 2
+    }
+
+    "run function well in case of right (to left)" in {
+      val atom = new AtomicInteger(1)
+      Ior.right[String, Int](1).some.doubleFlatTap(_ => Ior.left("bar").some) mustBe Some(Ior.left("bar"))
+      atom.get() mustBe 1
+    }
+
+    "run function well in case of right (to both)" in {
+      val atom = new AtomicInteger(1)
+      Ior.right[String, Int](1).some.doubleFlatTap(i => Ior.both("bar", atom.addAndGet(i)).some) mustBe Some(
+        Ior.both("bar", 1)
+      )
+      atom.get() mustBe 2
+    }
+
+    "run function well in case of left (to right)" in {
+      val atom = new AtomicInteger(1)
+      Ior.left[String, Int]("foo").some.doubleFlatTap(i => Ior.right(atom.addAndGet(i)).some) mustBe Some(
+        Ior.left("foo")
+      )
+      atom.get() mustBe 1
+    }
+
+    "run function well in case of left (to left)" in {
+      val atom = new AtomicInteger(1)
+      Ior.left[String, Int]("foo").some.doubleFlatTap(_ => Ior.left("bar").some) mustBe Some(Ior.left("foo"))
+      atom.get() mustBe 1
+    }
+
+    "run function well in case of left (to both)" in {
+      val atom = new AtomicInteger(1)
+      Ior.left[String, Int]("foo").some.doubleFlatTap(i => Ior.both("bar", atom.addAndGet(i)).some) mustBe Some(
+        Ior.left("foo")
+      )
+      atom.get() mustBe 1
+    }
+
+    "run function well in case of both (to right)" in {
+      val atom = new AtomicInteger(1)
+      Ior.both[String, Int]("foo", 1).some.doubleFlatTap(i => Ior.right(atom.addAndGet(i)).some) mustBe Some(
+        Ior.both("foo", 1)
+      )
+      atom.get() mustBe 2
+    }
+
+    "run function well in case of both (to left)" in {
+      val atom = new AtomicInteger(1)
+      Ior.both[String, Int]("foo", 1).some.doubleFlatTap(_ => Ior.left("bar").some) mustBe Some(Ior.left("foobar"))
+      atom.get() mustBe 1
+    }
+
+    "run function well in case of both (to both)" in {
+      val atom = new AtomicInteger(1)
+      Ior.both[String, Int]("foo", 1).some.doubleFlatTap(i => Ior.both("bar", atom.addAndGet(i)).some) mustBe Some(
+        Ior.both("foobar", 1)
+      )
+      atom.get() mustBe 2
+    }
+
+    "nothing happen in case of effect error" in {
+      Ior.right[String, Int](1).some.doubleFlatTap(_ => none[Ior[String, Any]]) mustBe none[Ior[String, Int]]
+      Ior.left[String, Int]("foo").some.doubleFlatTap(_ => none[Ior[String, Any]]) mustBe Some(Ior.left("foo"))
+      Ior.both[String, Int]("foo", 1).some.flatTapF(_ => none[Ior[String, Any]]) mustBe none[Ior[String, Int]]
+    }
+
+    "nothing happen in case of effect error (source)" in {
+
+      val atom = new AtomicInteger(1)
+      none[Ior[String, Int]].doubleFlatTap(i => Ior.right(atom.addAndGet(i)).some) mustBe none[Ior[String, Int]]
+      none[Ior[String, Int]].doubleFlatTap(i => Ior.left("bar").some) mustBe none[Ior[String, Int]]
+      none[Ior[String, Int]].doubleFlatTap(i => Ior.both("bar", atom.addAndGet(i)).some) mustBe none[Ior[String, Int]]
+      atom.get() mustBe 1
+    }
+
+    "check lazyness" in {
+      none[Ior[String, Int]].doubleFlatTap(_ => boom[Option[Ior[String, Any]]]) mustBe none[Ior[String, Int]]
+      Ior.left[String, Int]("foo").some.doubleFlatTap(_ => boom[Option[Ior[String, Any]]]) mustBe Some(Ior.left("foo"))
     }
   }
 }
