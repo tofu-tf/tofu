c010bb60927cbbd558a05284ca542983dd6f5e01
Nikita Vilunov <nikita@vilunov.me>
2022-02-10 12:12:05 +0300

    Add a tofu logging provider for logstash logback encoder

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -69,6 +69,8 @@ object Dependencies {
 
     val log4Cats = "1.5.1"
 
+    val logstashLogback = "7.0.1"
+
     val groovy = "3.0.9"
 
     val http4s = "0.22.10"
@@ -81,48 +83,49 @@ object Dependencies {
       ExclusionRule("org.typelevel", "cats-effect_2.13"),
       ExclusionRule("org.typelevel", "cats-effect_2.12")
     )
-  val catsCore         = "org.typelevel"              %% "cats-core"               % Version.cats
-  val catsFree         = "org.typelevel"              %% "cats-free"               % Version.cats
-  val catsMtl          = "org.typelevel"              %% "cats-mtl"                % Version.catsMtl
-  val monocle          = "com.github.julien-truffaut" %% "monocle-core"            % Version.monocle excludeAll (noCatsCore: _*)
-  val alleycats        = "org.typelevel"              %% "alleycats-core"          % Version.cats
-  val catsEffect2      = "org.typelevel"              %% "cats-effect"             % Version.catsEffect2
-  val catsEffect3      = "org.typelevel"              %% "cats-effect"             % Version.catsEffect3
-  val monix            = "io.monix"                   %% "monix"                   % Version.monix excludeAll (noCatsEffect: _*)
-  val logback          = "ch.qos.logback"              % "logback-classic"         % Version.logback
-  val slf4j            = "org.slf4j"                   % "slf4j-api"               % Version.slf4j     % Provided
-  val circeCore        = "io.circe"                   %% "circe-core"              % Version.circe excludeAll (noCatsCore: _*)
-  val circeJava8       = "io.circe"                   %% "circe-java8"             % Version.circe excludeAll (noCatsCore: _*)
-  val circeDerivation  = "io.circe"                   %% "circe-derivation"        % Version.circe excludeAll (noCatsCore: _*)
-  val magnolia         = "com.propensive"             %% "magnolia"                % Version.magnolia
-  val derevo           = "tf.tofu"                    %% "derevo-core"             % Version.derevo
-  val derevoCirce      = "tf.tofu"                    %% "derevo-circe"            % Version.derevo
-  val derevoTagless    = "tf.tofu"                    %% "derevo-cats-tagless"     % Version.derevo
-  val enumeratum       = "com.beachape"               %% "enumeratum"              % Version.enumeratum
-  val fs2              = "co.fs2"                     %% "fs2-io"                  % Version.fs2
-  val tethys           = "com.tethys-json"            %% "tethys-core"             % Version.tethys
-  val tethysJackson    = "com.tethys-json"            %% "tethys-jackson"          % Version.tethys
-  val catsTagless      = "org.typelevel"              %% "cats-tagless-macros"     % Version.catsTagless
-  val typesafeConfig   = "com.typesafe"                % "config"                  % Version.typesafeConfig
-  val zio              = "dev.zio"                    %% "zio"                     % Version.zio
-  val zioCats          = "dev.zio"                    %% "zio-interop-cats"        % Version.zioCats
-  val scalatest        = "org.scalatest"              %% "scalatest"               % Version.scalatest % Test
-  val shapeless        = "com.chuusai"                %% "shapeless"               % Version.shapeless
-  val refined          = "eu.timepit"                 %% "refined"                 % Version.refined
-  val doobieCore       = "org.tpolecat"               %% "doobie-core"             % Version.doobie
-  val doobieH2         = "org.tpolecat"               %% "doobie-h2"               % Version.doobie
-  val doobieCoreCE3    = "org.tpolecat"               %% "doobie-core"             % Version.doobieCE3
-  val doobieH2CE3      = "org.tpolecat"               %% "doobie-h2"               % Version.doobieCE3
-  val collectionCompat = "org.scala-lang.modules"     %% "scala-collection-compat" % Version.collectionCompat
-  val log4Cats         = "org.typelevel"              %% "log4cats-core"           % Version.log4Cats
-  val groovy           = "org.codehaus.groovy"         % "groovy"                  % Version.groovy
+  val catsCore         = "org.typelevel"              %% "cats-core"                % Version.cats
+  val catsFree         = "org.typelevel"              %% "cats-free"                % Version.cats
+  val catsMtl          = "org.typelevel"              %% "cats-mtl"                 % Version.catsMtl
+  val monocle          = "com.github.julien-truffaut" %% "monocle-core"             % Version.monocle excludeAll (noCatsCore: _*)
+  val alleycats        = "org.typelevel"              %% "alleycats-core"           % Version.cats
+  val catsEffect2      = "org.typelevel"              %% "cats-effect"              % Version.catsEffect2
+  val catsEffect3      = "org.typelevel"              %% "cats-effect"              % Version.catsEffect3
+  val monix            = "io.monix"                   %% "monix"                    % Version.monix excludeAll (noCatsEffect: _*)
+  val logback          = "ch.qos.logback"              % "logback-classic"          % Version.logback
+  val slf4j            = "org.slf4j"                   % "slf4j-api"                % Version.slf4j     % Provided
+  val circeCore        = "io.circe"                   %% "circe-core"               % Version.circe excludeAll (noCatsCore: _*)
+  val circeJava8       = "io.circe"                   %% "circe-java8"              % Version.circe excludeAll (noCatsCore: _*)
+  val circeDerivation  = "io.circe"                   %% "circe-derivation"         % Version.circe excludeAll (noCatsCore: _*)
+  val magnolia         = "com.propensive"             %% "magnolia"                 % Version.magnolia
+  val derevo           = "tf.tofu"                    %% "derevo-core"              % Version.derevo
+  val derevoCirce      = "tf.tofu"                    %% "derevo-circe"             % Version.derevo
+  val derevoTagless    = "tf.tofu"                    %% "derevo-cats-tagless"      % Version.derevo
+  val enumeratum       = "com.beachape"               %% "enumeratum"               % Version.enumeratum
+  val fs2              = "co.fs2"                     %% "fs2-io"                   % Version.fs2
+  val tethys           = "com.tethys-json"            %% "tethys-core"              % Version.tethys
+  val tethysJackson    = "com.tethys-json"            %% "tethys-jackson"           % Version.tethys
+  val catsTagless      = "org.typelevel"              %% "cats-tagless-macros"      % Version.catsTagless
+  val typesafeConfig   = "com.typesafe"                % "config"                   % Version.typesafeConfig
+  val zio              = "dev.zio"                    %% "zio"                      % Version.zio
+  val zioCats          = "dev.zio"                    %% "zio-interop-cats"         % Version.zioCats
+  val scalatest        = "org.scalatest"              %% "scalatest"                % Version.scalatest % Test
+  val shapeless        = "com.chuusai"                %% "shapeless"                % Version.shapeless
+  val refined          = "eu.timepit"                 %% "refined"                  % Version.refined
+  val doobieCore       = "org.tpolecat"               %% "doobie-core"              % Version.doobie
+  val doobieH2         = "org.tpolecat"               %% "doobie-h2"                % Version.doobie
+  val doobieCoreCE3    = "org.tpolecat"               %% "doobie-core"              % Version.doobieCE3
+  val doobieH2CE3      = "org.tpolecat"               %% "doobie-h2"                % Version.doobieCE3
+  val collectionCompat = "org.scala-lang.modules"     %% "scala-collection-compat"  % Version.collectionCompat
+  val log4Cats         = "org.typelevel"              %% "log4cats-core"            % Version.log4Cats
+  val logstashLogback  = "net.logstash.logback"        % "logstash-logback-encoder" % Version.logstashLogback
+  val groovy           = "org.codehaus.groovy"         % "groovy"                   % Version.groovy
   val http4s           =
     Seq("org.http4s" %% "http4s-dsl", "org.http4s" %% "http4s-circe", "org.http4s" %% "http4s-blaze-server").map(
       _ % Version.http4s
     )
   // Compile-time only
-  val macroParadise    = "org.scalamacros"             % "paradise"                % Version.macroParadise cross CrossVersion.patch
-  val kindProjector    = "org.typelevel"              %% "kind-projector"          % Version.kindProjector cross CrossVersion.patch
-  val simulacrum       = "org.typelevel"              %% "simulacrum"              % Version.simulacrum
-  val betterMonadicFor = "com.olegpy"                 %% "better-monadic-for"      % Version.betterMonadicFor
+  val macroParadise    = "org.scalamacros"             % "paradise"                 % Version.macroParadise cross CrossVersion.patch
+  val kindProjector    = "org.typelevel"              %% "kind-projector"           % Version.kindProjector cross CrossVersion.patch
+  val simulacrum       = "org.typelevel"              %% "simulacrum"               % Version.simulacrum
+  val betterMonadicFor = "com.olegpy"                 %% "better-monadic-for"       % Version.betterMonadicFor
 }
