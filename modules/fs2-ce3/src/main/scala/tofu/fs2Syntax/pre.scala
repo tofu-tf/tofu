a423432e5c21c2310ddeba59411f248f309b7732
oskin1 <ilya.arcadich@gmail.com>
2022-05-06 12:48:08 +0300

    Fix deprecation warning.

--- modules/fs2-ce3/src/main/scala/tofu/fs2Syntax/pre.scala
+++ modules/fs2-ce3/src/main/scala/tofu/fs2Syntax/pre.scala
@@ -8,6 +8,6 @@ import tofu.syntax.funk.funK
 object pre {
   implicit final class TofuPreStreamSyntax[F[_], U[f[_]]](private val self: U[T[F, *]]) extends AnyVal {
     def attachStream(alg: U[Stream[F, *]])(implicit U: ApplyK[U]): U[Stream[F, *]] =
-      U.map2K(self, alg)(funK(t2k => Stream.eval_(t2k.first.value) ++ t2k.second))
+      U.map2K(self, alg)(funK(t2k => Stream.exec(t2k.first.value) ++ t2k.second))
   }
 }
