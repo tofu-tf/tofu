a1ca20212a295e5e72f5c0274deab77784769859
Ji.Liu <ji.liu@convertlab.com>
2023-04-07 21:14:39 +0800

    feat: +invariantK instance for Mid

--- modules/kernel/higherKind/src/test/scala-2/tofu/higherKind/MidSuite.scala
+++ modules/kernel/higherKind/src/test/scala-2/tofu/higherKind/MidSuite.scala
@@ -1,6 +1,6 @@
 package tofu.higherKind
 
-import cats.tagless.ApplyK
+import cats.tagless.{ApplyK, InvariantK}
 import cats.{Monoid, Semigroup}
 import tofu.higherKind.Mid._
 
@@ -8,6 +8,7 @@ object MidSuite {
   def summonMidInstances[F[_], U[_[_]]: ApplyK, W[_[_]]: MonoidalK](): Unit = {
     implicitly[Semigroup[U[Mid[F, *]]]]
     implicitly[Monoid[W[Mid[F, *]]]]
+    implicitly[InvariantK[Mid[*[_], Any]]]
     ()
   }
 }
