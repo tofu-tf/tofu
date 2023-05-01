3ea834f0439aaaa7af50a4d2927cef21d31f373d
Odomontois <odomontois@gmail.com>
2023-05-01 17:23:03 +0200

    Upgrade Docusaurus, Fix Dox link (#1121)
    * upgrade docusaurus
    
    * Fixed doc link

--- website/src/components/ImageSwitcher.js
+++ website/src/components/ImageSwitcher.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import useThemeContext from '@theme/hooks/useThemeContext'; //docs: https://v2.docusaurus.io/docs/2.0.0-alpha.69/theme-classic#usethemecontext
+import { useColorMode } from '@docusaurus/theme-common'; //docs: https://v2.docusaurus.io/docs/2.0.0-alpha.69/theme-classic#usethemecontext
 
-const ImageSwitcher = ({lightImageSrc, darkImageSrc, imageHeight}) => {
-  const { isDarkTheme } = useThemeContext();
+const ImageSwitcher = ({ lightImageSrc, darkImageSrc, imageHeight }) => {
+    const { isDarkTheme } = useColorMode();
 
-  return (
-    <img src={isDarkTheme ? darkImageSrc : lightImageSrc} height={imageHeight} />
-  )
+    return (
+        <img src={isDarkTheme ? darkImageSrc : lightImageSrc} height={imageHeight} />
+    )
 }
 
 export default ImageSwitcher;
