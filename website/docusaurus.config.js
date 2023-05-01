f330178f6e212c536169dd4c619008b41e3160b2
Oleg Nizhnikov <odomontois@gmail.com>
2023-05-01 17:52:59 +0200

    fix link to retain contextual path

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -73,7 +73,7 @@ module.exports = {
           position: "right"
         },
         {
-          href: "pathname:///api/index.html",
+          href: "pathname:///api/",
           label: "API",
           position: "right"
         }
