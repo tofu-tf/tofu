c2125200b88d083237691ec94cb116c926fe7713
Vladislav Ugryumov <vlad.ugrum@gmail.com>
2023-11-22 03:26:02 +0300

    Fixed cross compile warnings.

--- modules/core/ce2/src/test/scala/tofu/ScopedSuite.scala
+++ modules/core/ce2/src/test/scala/tofu/ScopedSuite.scala
@@ -6,14 +6,13 @@ import scala.concurrent.ExecutionContext
 import cats.effect.Blocker
 import tofu.syntax.scoped.*
 import tofu.syntax.monadic.*
-import scala.annotation.nowarn
+import tofu.compat.unused
 import scala.concurrent.Future
 
 class ScopedSuite {
-  @nowarn("cat=unused-params")
   def doSomething[F[_]: ContextShift: Async, A](fa: F[A], ea: => A)(calcEc: ExecutionContext)(implicit
-      ec: ExecutionContext,
-      block: Blocker
+      @unused ec: ExecutionContext,
+      @unused block: Blocker
   ): F[List[A]] = {
     implicit val exec: CalcExec[F] = Scoped.makeExecuteCE2(calcEc)
 
@@ -31,7 +30,7 @@ class ScopedSuite {
       a4 <- calculation(fa)
 
       // test for CalcExec[F], outer ec is shadowed
-      a5 <- deferCalcFuture[F](implicit ec => Future(ea))
+      _ <- deferCalcFuture[F](implicit ec => Future(ea))
     } yield List(a1, a2, a3, a4)
   }
 }
