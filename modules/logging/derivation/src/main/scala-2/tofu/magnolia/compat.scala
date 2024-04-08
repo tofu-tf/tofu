ea518dc8f8d49e43fa61e8ab62d1071270e4fc4b
Vadim Chelyshov <qtankle@gmail.com>
2024-04-08 20:28:05 +0300

    fmt

--- modules/logging/derivation/src/main/scala-2/tofu/magnolia/compat.scala
+++ modules/logging/derivation/src/main/scala-2/tofu/magnolia/compat.scala
@@ -3,7 +3,6 @@ package tofu.magnolia
 object compat {
   type Param[Typeclass[_], Type] = magnolia1.Param[Typeclass, Type]
 
-  def deref[Typeclass[_], Type](p: Param[Typeclass, Type]): Type => p.PType = tpe =>
-    p.dereference(tpe)
+  def deref[Typeclass[_], Type](p: Param[Typeclass, Type]): Type => p.PType = tpe => p.dereference(tpe)
 
 }
--- modules/logging/derivation/src/main/scala-3/tofu/magnolia/compat.scala
+++ modules/logging/derivation/src/main/scala-3/tofu/magnolia/compat.scala
@@ -3,7 +3,6 @@ package tofu.magnolia
 object compat {
   type Param[Typeclass[_], Type] = magnolia1.CaseClass.Param[Typeclass, Type]
 
-  def deref[Typeclass[_], Type](p: Param[Typeclass, Type]): Type => p.PType = tpe =>
-    p.deref(tpe)
+  def deref[Typeclass[_], Type](p: Param[Typeclass, Type]): Type => p.PType = tpe => p.deref(tpe)
 
 }
