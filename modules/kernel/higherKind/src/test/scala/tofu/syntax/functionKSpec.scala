1778eacfafe5c537cd451c237e7add04d6374c3d
road21 <amtroitskiy@gmail.com>
2024-09-17 22:17:08 +0300

    fix warnings

--- modules/kernel/higherKind/src/test/scala/tofu/syntax/functionKSpec.scala
+++ modules/kernel/higherKind/src/test/scala/tofu/syntax/functionKSpec.scala
@@ -2,18 +2,25 @@ package tofu.syntax
 
 import org.scalatest.funsuite.AnyFunSuite
 import cats.{Id, ~>}
-import tofu.syntax.funk._
 
 class functionKSpec extends AnyFunSuite {
-  test("funK infers type args") {
-    def infer(x: Id ~> Option): Unit = ()
+  def infer1(x: Id ~> Option): Unit = ()
 
-    assertCompiles("infer(funK(Some(_)))")
+  test("funK infers type args") {
+    assertCompiles(
+      """|import tofu.syntax.funk._
+         |infer1(funK(Some(_)))
+         |""".stripMargin
+    )
   }
 
-  test("funKFrom infers type args") {
-    def infer[F[_]](x: F ~> Option): Unit = ()
+  def infer2[F[_]](x: F ~> Option): Unit = ()
 
-    assertCompiles("infer(funKFrom[Id](Some(_)))")
+  test("funKFrom infers type args") {
+    assertCompiles(
+      """|import tofu.syntax.funk._
+         |infer2(funKFrom[Id](Some(_)))
+         |""".stripMargin
+    )
   }
 }
