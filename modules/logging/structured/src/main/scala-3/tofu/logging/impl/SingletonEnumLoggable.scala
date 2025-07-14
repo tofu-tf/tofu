5f3662ca12ce8dc4d3320aeae11c4411b2a9638a
Equwece <99554597+Equwece@users.noreply.github.com>
2025-07-14 23:02:48 +0400

    Collect enum case names in compile time

--- modules/logging/structured/src/main/scala-3/tofu/logging/impl/SingletonEnumLoggable.scala
+++ modules/logging/structured/src/main/scala-3/tofu/logging/impl/SingletonEnumLoggable.scala
@@ -5,13 +5,17 @@ import scala.deriving.Mirror
 trait SingletonEnumLoggable[A] extends SingleValueLoggable[A]
 
 object SingletonEnumLoggable:
-  def makeInstance[T]: SingletonEnumLoggable[T] =
+  private def makeInstance[T: Mirror.SumOf](enumCaseNames: List[String]): SingletonEnumLoggable[T] =
+    def getCaseName[T](v: T)(using m: Mirror.SumOf[T]): String =
+      val ordIndex = m.ordinal(v)
+      enumCaseNames(ordIndex)
+
     new SingletonEnumLoggable[T]:
-      def logValue(a: T): LogParamValue = StrValue(a.toString)
+      def logValue(a: T): LogParamValue = StrValue(getCaseName(a))
 
       override def putField[I, V, R, M](a: T, name: String, input: I)(implicit receiver: LogRenderer[I, V, R, M]): R =
-        receiver.addString(name, a.toString, input)
+        receiver.addString(name, getCaseName(a), input)
 
   inline def derived[T: Mirror.SumOf]: SingletonEnumLoggable[T] =
-    SingletonEnumLoggableMacro.ensureSingletonEnum[T]
-    makeInstance[T]
+    val enumCaseNames = SingletonEnumLoggableMacro.getSingletonEnumCases[T]
+    makeInstance[T](enumCaseNames)
--- modules/logging/structured/src/main/scala-3/tofu/logging/impl/SingletonEnumLoggableMacro.scala
+++ modules/logging/structured/src/main/scala-3/tofu/logging/impl/SingletonEnumLoggableMacro.scala
@@ -4,18 +4,19 @@ import scala.quoted.*
 
 object SingletonEnumLoggableMacro:
 
-  inline def ensureSingletonEnum[T]: Unit =
-    ${ ensureSingletonEnumMacro[T] }
+  inline def getSingletonEnumCases[T]: List[String] =
+    ${ getSingletonEnumCases[T] }
 
-  def ensureSingletonEnumMacro[T: Type](using quotes: Quotes): Expr[Unit] =
+  def getSingletonEnumCases[T: Type](using quotes: Quotes): Expr[List[String]] =
     import quotes.reflect.*
 
     val enumSymbol = TypeRepr.of[T].typeSymbol
 
-    enumSymbol.children.foreach: caseSymbol =>
+    val enumCaseNames = enumSymbol.children.map: caseSymbol =>
       if (caseSymbol.caseFields.nonEmpty) then
         report.errorAndAbort(
           s"${enumSymbol.fullName} has non-singleton case ${caseSymbol.name}, therefore SingletonEnumLoggable instance can not be derived"
         )
+      caseSymbol.name
 
-    '{ () }
+    Expr(enumCaseNames)
